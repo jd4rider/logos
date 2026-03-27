f875560dbd20ea53ec10c54261b6d414f209ea1a
Jonathan Forrider <jd4rider@gmail.com>
2026-03-27 10:58:13 -0600

    Render package manager manifests for v0.1.5

--- packaging/homebrew/Casks/logos-ai.rb
+++ packaging/homebrew/Casks/logos-ai.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "logos-ai" do
-  version "0.1.4"
-  sha256 "eab8350048cf2b7e53a3c381cd93840d99efdbc3e42bbb02e32b981a997abf83"
+  version "0.1.5"
+  sha256 "98fe2b659e45e8697cd6f3aa00a1790ed60fa946d8868d08afdbf5ac4f76a1df"
 
   url "https://github.com/jd4rider/logos-releases/releases/download/v#{version}/logos-ai-macos-universal.tar.gz"
   name "Logos AI"
